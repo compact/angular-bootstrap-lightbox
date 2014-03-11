937c0cb42eef468505f679396568a0885c98b851
compact <adjklx@gmail.com>
2014-03-11 12:43:59 -0400

    Gruntfile: remove date from uglify banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.name %> */\n'
       },
       jid: {
         files: {
