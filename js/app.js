b0f09ba8625296b9f2e200afbfb70c64b332904e
compact <adjklx@gmail.com>
2014-08-22 05:13:41 -0400

    add more sample images

--- js/app.js
+++ js/app.js
@@ -28,7 +28,31 @@ angular.module('app').controller('GalleryCtrl', function ($scope, Lightbox) {
       'width': 50,
       'height': 33,
       'thumbUrl': 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/%28%2B%29-CAMP-3D-spacefill.png/50px-%28%2B%29-CAMP-3D-spacefill.png',
-      'caption': '50x33. The min modal dimensions (400x200) apply.'
+      'caption': '50x33. The default minimum modal dimensions (400x200) apply.'
+    },
+    {
+      'url': 'https://upload.wikimedia.org/wikipedia/en/4/4a/Bowen_America.png',
+      'width': 1722,
+      'height': 1347,
+      'thumbUrl': 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Bowen_America.png/120px-Bowen_America.png'
+    },
+    {
+      'url': 'https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Jaume_Mercad%C3%A9_Queralt_-_Outskirts_of_Valls_-_Google_Art_Project.jpg/907px-Jaume_Mercad%C3%A9_Queralt_-_Outskirts_of_Valls_-_Google_Art_Project.jpg',
+      'width': 907,
+      'height': 1024,
+      'thumbUrl': 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Jaume_Mercad%C3%A9_Queralt_-_Landscape_in_Valls_-_Google_Art_Project.jpg/103px-Jaume_Mercad%C3%A9_Queralt_-_Landscape_in_Valls_-_Google_Art_Project.jpg'
+    },
+    {
+      'url': 'https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Joan_Colom_Agust%C3%AD_-_Estaci%C3%B3_de_Fran%C3%A7a_-_Google_Art_Project.jpg/1256px-Joan_Colom_Agust%C3%AD_-_Estaci%C3%B3_de_Fran%C3%A7a_-_Google_Art_Project.jpg',
+      'width': 1256,
+      'height': 1024,
+      'thumbUrl': 'https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Joan_Colom_Agust%C3%AD_-_Estaci%C3%B3_de_Fran%C3%A7a_-_Google_Art_Project.jpg/120px-Joan_Colom_Agust%C3%AD_-_Estaci%C3%B3_de_Fran%C3%A7a_-_Google_Art_Project.jpg'
+    },
+    {
+      'url': 'https://upload.wikimedia.org/wikipedia/en/d/dd/Nicola_Fabricatore_-_Last_Purchases_-_Google_Art_Project.jpg',
+      'width': 4369,
+      'height': 3225,
+      'thumbUrl': 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Nicola_Fabricatore_-_Last_Purchases_-_Google_Art_Project.jpg/120px-Nicola_Fabricatore_-_Last_Purchases_-_Google_Art_Project.jpg'
     }
   ];
 
